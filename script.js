6b99b2a866988d4c606554b6cb4755f535bb72b1
shyye <emma.art.dev@gmail.com>
2024-12-03 12:12:19 +0100

    Add instagram posts and links

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const projects = [
     {
         name: "2DT304-Project-with-Embedded-System",
         description: "Student project about lifebuoy monitoring to ensure that lifebuoys are in the correct positions or discover if they are gone.",
-        image: "images\\2DT304-Project-with-Embedded-System.jpg", 
+        image: "assets/images/2DT304-Project-with-Embedded-System.jpg", 
         url: "https://github.com/shyye/2DT304-Project-with-Embedded-System",
         languages: [
             { name: "C", class: "c" },
@@ -14,7 +14,17 @@ const projects = [
     {
         name: "Migraine Management Platform [Thesis work]",
         description: "Migraine Management Platform: Bridging Mobile, Wearable Tracking, and Treatment Devices. This degree project investigates integrating a mobile application with a prototype treatment device and health data from smartwatches, with smartwatch integration explored only in theory. It focuses on Bluetooth communication, evaluates Flutter development, and delivers two apps: one experimental prototype and one layout concept.",
-        image: "images\\thesis-work-color.png",
+        image: "assets/images/thesis-work-color.png",
+        url: "https://www.diva-portal.org/smash/record.jsf?pid=diva2:1891301",
+        languages: [
+            { name: "Flutter", class: "flutter" }
+        ]
+    },
+
+    {
+        name: "Migraine Management Platform [Thesis work]",
+        description: "Migraine Management Platform: Bridging Mobile, Wearable Tracking, and Treatment Devices. This degree project investigates integrating a mobile application with a prototype treatment device and health data from smartwatches, with smartwatch integration explored only in theory. It focuses on Bluetooth communication, evaluates Flutter development, and delivers two apps: one experimental prototype and one layout concept.",
+        image: "assets/images/thesis-work-color.png",
         url: "https://www.diva-portal.org/smash/record.jsf?pid=diva2:1891301",
         languages: [
             { name: "Flutter", class: "flutter" }
@@ -72,3 +82,71 @@ function renderProjects() {
 // Call the function on page load
 //   document.addEventListener("DOMContentLoaded", renderProjects);
 renderProjects();
+
+
+//TODO: move to seperate js file
+// List of Instagram posts with attributes
+const instagramPosts = [
+    {
+        imagePath: "assets/images-instagram/instagramv51_1.jpg",
+        url: "https://www.instagram.com/dataitlnu/p/CIxsUJcpps1/?img_index=1"
+    },
+    {
+        imagePath: "assets/images-instagram/instagramv51_2-new.jpg",
+        url: "https://www.instagram.com/dataitlnu/p/CI0AZhVJAso/?img_index=1"
+    },
+    {
+        imagePath: "assets/images-instagram/instagramv51_3.jpg",
+        url: "https://www.instagram.com/dataitlnu/p/CI2ZbyqJmh_/?img_index=1"
+    },
+    {
+        imagePath: "assets/images-instagram/instagramv51_4.jpg",
+        url: "https://www.instagram.com/dataitlnu/p/CI5e2mypAQd/"
+    },
+    {
+        imagePath: "assets/images-instagram/instagramv51_5.jpg",
+        url: "https://www.instagram.com/dataitlnu/p/CI7jTjophy7/"
+    },
+    {
+        imagePath: "assets/images-instagram/instagramv51_6.jpg",
+        url: "https://www.instagram.com/dataitlnu/p/CI_NuaHpsCM/?img_index=1"
+    },
+    {
+        imagePath: "assets/images-instagram/instagramv51_7-1.jpg",
+        url: "https://www.instagram.com/dataitlnu/p/CJBTxTQpBkb/?img_index=1"
+    },
+    {
+        imagePath: "assets/images-instagram/instagramv51_7.jpg",
+        url: "https://www.instagram.com/dataitlnu/p/CJBTxTQpBkb/?img_index=1"
+    }    
+];
+
+// Dynamically render posts
+const renderPosts = () => {
+    const container = document.querySelector("#instagram-wrapper");
+    instagramPosts.forEach(post => {
+        // Create a link element that wraps the entire post
+        const link = document.createElement("a");
+        link.href = post.url;
+        link.target = "_blank";
+        link.className = "instagram-item-wrapper";
+
+        // Create an image element
+        const img = document.createElement("img");
+        img.src = post.imagePath;
+        img.alt = "Instagram Post";
+        img.className = "instagram-image-wrapper";
+
+        // Append image to the link
+        link.append(img);
+
+        // Append the link to the container
+        container.append(link);
+    });
+};
+
+// Run the render function after DOM content is loaded
+// document.addEventListener("DOMContentLoaded", renderPosts);
+renderPosts();
+
+
